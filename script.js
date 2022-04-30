86ce5df51573e8afaa1eb7d09a9be035ede44d37
joksa19 <nikolajoksimovic95@gmail.com>
2022-04-30 14:04:28 +0200

    Drop menu closes when link is targeted.

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-const navToggleBtnEl = document.querySelector(".navbar-toggle-btn");
-const navDropMenuEl = document.querySelector(".navbar-dropmenu");
+const navToggleBtnEl = document.querySelector(".navbar-toggle-btn"),
+    navDropMenuEl = document.querySelector(".navbar-dropmenu"),
+    linksEl = document.querySelectorAll(".link");
+
+// NAVBAR DROP MENU
 let isDropMenuDown = false;
     
+window.addEventListener("resize", function(e){
+    navDropMenuEl.style.transition = "all 0.0s"
+    const setHeight = document.querySelector(".links").clientHeight;
+    if(this.window.innerWidth <= 1280){
+        renderDropMenu();
+    }else{
+        isDropMenuDown = false;
+    }
+})
+
 navToggleBtnEl.addEventListener("click", function(){
     navDropMenuEl.style.transition = "all 0.1s linear"
     isDropMenuDown = !isDropMenuDown;
-    const setHeight = document.querySelector(".links").clientHeight;
+    renderDropMenu();
+})
+
+linksEl.forEach(function(item){
+    item.addEventListener("click", function(e){
+        // console.log("click");
+        isDropMenuDown = false;
+        renderDropMenu();
+    })
+})
+
+function renderDropMenu(){
     
+    const setHeight = document.querySelector(".links").clientHeight;
     if(isDropMenuDown){
         navDropMenuEl.style.height = `${setHeight}px`
     }else{
         navDropMenuEl.style.height = `0px`
     }
-})
-
-window.addEventListener("resize", function(e){
-    navDropMenuEl.style.transition = "all 0.0s"
-    const setHeight = document.querySelector(".links").clientHeight;
-    if(this.window.innerWidth <= 1280 && isDropMenuDown){
-        navDropMenuEl.style.height = `${setHeight}px`
-    }else{
-        isDropMenuDown = false;
-        navDropMenuEl.style.height = `0px`
-    }
-})
-
+}
