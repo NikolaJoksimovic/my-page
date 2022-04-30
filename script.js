1edd3498588e3eb7f067edecd736906324206237
joksa19 <nikolajoksimovic95@gmail.com>
2022-04-30 13:51:34 +0200

    Fixed some problems with navbar transitioning..

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const navToggleBtnEl = document.querySelector(".navbar-toggle-btn");
-let navDropMenuEl = document.querySelector(".navbar-dropmenu");
-
+const navDropMenuEl = document.querySelector(".navbar-dropmenu");
+let isDropMenuDown = false;
     
 navToggleBtnEl.addEventListener("click", function(){
+    navDropMenuEl.style.transition = "all 0.1s linear"
+    isDropMenuDown = !isDropMenuDown;
     const setHeight = document.querySelector(".links").clientHeight;
-    const dropMenuCurrentHeight = navDropMenuEl.clientHeight;
-    if(dropMenuCurrentHeight === 0){
+    
+    if(isDropMenuDown){
         navDropMenuEl.style.height = `${setHeight}px`
     }else{
         navDropMenuEl.style.height = `0px`
     }
 })
+
 window.addEventListener("resize", function(e){
+    navDropMenuEl.style.transition = "all 0.0s"
     const setHeight = document.querySelector(".links").clientHeight;
-    if(this.window.innerWidth <= 1280 && navDropMenuEl.clientHeight !== 0){
+    if(this.window.innerWidth <= 1280 && isDropMenuDown){
         navDropMenuEl.style.height = `${setHeight}px`
+    }else{
+        isDropMenuDown = false;
+        navDropMenuEl.style.height = `0px`
     }
 })
+
