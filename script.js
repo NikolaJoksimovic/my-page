47a46539b24d7c2d648ebcf481a7dc64142e3afc
joksa19 <nikolajoksimovic95@gmail.com>
2022-04-30 15:24:52 +0200

    Added go-up button and smooth scrolling.

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const navToggleBtnEl = document.querySelector(".navbar-toggle-btn"),
     navDropMenuEl = document.querySelector(".navbar-dropmenu"),
-    linksEl = document.querySelectorAll(".link");
+    navDropMenuContainerEl = document.querySelector(".navbar-container"),
+    linksEl = document.querySelectorAll(".link"),
+    navEl = document.querySelector("nav"),
+    upBtnEl = document.querySelector(".up-btn");
 
 // NAVBAR DROP MENU
 let isDropMenuDown = false;
-    
-window.addEventListener("resize", function(e){
+
+window.addEventListener("scroll", function(){
+    const scrollHeight = this.window.scrollY;
+    const navHeight = navEl.clientHeight;
+    if(scrollHeight > navHeight){
+        navEl.classList.add("nav-fixed");
+        navDropMenuContainerEl.classList.add("navbar-container-fixed");
+    }else{
+        navEl.classList.remove("nav-fixed");
+        navDropMenuContainerEl.classList.remove("navbar-container-fixed");
+    }
+    if(scrollHeight > (this.window.innerHeight)/2){
+        upBtnEl.classList.add("up-btn-show");
+    }else{
+        upBtnEl.classList.remove("up-btn-show");
+    }
+})
+
+window.addEventListener("resize", function(){
     navDropMenuEl.style.transition = "all 0.0s"
     const setHeight = document.querySelector(".links").clientHeight;
     if(this.window.innerWidth <= 1280){
@@ -15,14 +35,8 @@ window.addEventListener("resize", function(e){
     }
 })
 
-navToggleBtnEl.addEventListener("click", function(){
-    navDropMenuEl.style.transition = "all 0.1s linear"
-    isDropMenuDown = !isDropMenuDown;
-    renderDropMenu();
-})
-
 linksEl.forEach(function(item){
-    item.addEventListener("click", function(e){
+    item.addEventListener("click", function(){
         navDropMenuEl.style.transition = "all 0.0s";
         isDropMenuDown = false;
         renderDropMenu();
@@ -37,3 +51,15 @@ function renderDropMenu(){
         navDropMenuEl.style.height = `0px`
     }
 }
+
+// FUNCIONAL BUTTONS
+
+navToggleBtnEl.addEventListener("click", function(){
+    navDropMenuEl.style.transition = "all 0.1s linear"
+    isDropMenuDown = !isDropMenuDown;
+    renderDropMenu();
+})
+
+upBtnEl.addEventListener("click", function(){
+    document.documentElement.scrollTop = 0;
+})
